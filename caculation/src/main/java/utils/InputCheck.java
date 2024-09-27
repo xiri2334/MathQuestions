b0c90c64d7ca282e05a32159278b0c8cd7eb1977
xiri2334 <2303267297@qq.com>
2024-09-27 21:41:53 +0800

    Update InputCheck.java

--- caculation/src/main/java/utils/InputCheck.java
+++ caculation/src/main/java/utils/InputCheck.java
@@ -10,26 +10,17 @@ public class InputCheck {
      * @param command
      * @return: 返回定好顺序的命令数组
      */
-    public static String[] checkParams(String command) {
+   public static String[] checkParams(String command) {
         String[] s = command.split(" ");
-        //其实只能输入长度为4的指令
-        if (s.length == 4){
-            String first = s[0];String third = s[2];String second = s[1];String fourth = s[3];
-            //1. -n i -r j / -r i -n j
-            if(("-n".equals(first)&& "-r".equals(third)) || ("-e".equals(first)&& "-a".equals(third))){
-                return s;
-            }
-            if(("-r".equals(first)&& "-n".equals(third)) || ("-a".equals(first)&& "-e".equals(third))){
-                s[0] = third;
-                s[1] = fourth;
-                s[2] = first;
-                s[3] = second;
-                return s;
-            }
-            return null;
-        }else {
-            return null;
+
+        if (s.length == 4) {
+            String first = s[0];
+            String third = s[2];
+            
+            if (("-n".equals(first) && "-r".equals(third)) || ("-e".equals(first) && "-a".equals(third))) return s;
+            else return null;
         }
+        return  null;
     }
 
     /*
