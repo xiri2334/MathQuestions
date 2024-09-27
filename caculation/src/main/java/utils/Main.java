5ca76d27591e36fd90df4cfb63b112a5e4317544
xiri2334 <2303267297@qq.com>
2024-09-27 21:50:20 +0800

    Update Main.java

--- caculation/src/main/java/utils/Main.java
+++ caculation/src/main/java/utils/Main.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 
         System.out.println("---------------欢迎使用四则运算题目生成程序---------------");
-        System.out.println("---------------使用-n 参数控制题目生成数目 -r 参数控制题目中数值（自然数、真分数和真分数分母）的范围---------------");
+        System.out.println("---------------使用-n 参数控制题目生成数目 -r 参数控制题目中数值范围---------------");
         System.out.println("---------------使用-e Exercises.txt -a Answers.txt 检查答案的正确率---------------");
         System.out.println("---------------退出请输入  Exit    ---------------");
         while(true){
@@ -21,26 +21,24 @@ public class Main {
             String[] s = InputCheck.checkParams(command);
             if (s !=null){
                 if ("-n".equals(s[0])) {
-                    int num = Integer.parseInt(s[1]);
-                    if (num <= 0) {
+                    if (Integer.parseInt(s[1]) <= 0) {
                         System.out.println("-n参数输入错误，请重新输入");
                         break;
                     }
                     else {
-                        int round = Integer.parseInt(s[3]);
-                        if (round <= 0) {
+                        if (Integer.parseInt(s[3]) <= 0) {
                             System.out.println("-r参数输入错误，请重新输入");
                         } else {
                             //获取运算式数组
-                            Map<String, String> questionAndResultMap = Expression.generate(num, round);
-                            File.printExerciseFileAndAnswerFile(questionAndResultMap);
+                            Map<String, String> questionAndResultMap = Expression.generate(Integer.parseInt(s[1]), Integer.parseInt(s[3]));
+                            File.WriteFile(questionAndResultMap);
                         }
                     }
                 }else {
                     String exerciseFileUrl = s[1];
                     String answerFileUrl = s[3];
                     //验证答案
-                    File.validateAnswerFile(exerciseFileUrl, answerFileUrl);
+                    File.MatchAnswerFile(exerciseFileUrl, answerFileUrl);
                 }
             }else {
                 System.out.println("参数输入有误，请重新输入");
